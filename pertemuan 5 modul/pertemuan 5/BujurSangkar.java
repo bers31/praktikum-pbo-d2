7c1cb7e7ef6f8b733ee9a319a253e21c3ecf038a
bers31 <126935803+bers31@users.noreply.github.com>
2024-03-21 11:35:57 +0700

    Add files via upload

--- pertemuan 5 modul/pertemuan 5/BujurSangkar.java
+++ pertemuan 5 modul/pertemuan 5/BujurSangkar.java
@@ -4,3 +4,14 @@ public class BujurSangkar extends BangunDatar{
         return luas;
     }
 }
+/*
+ *  Jika kelas BujurSangkar tidak membuat implementasi untuk metode abstrak hitungLuas 
+ *  pada kelas BangunDatar, maka BujurSangkar akan menjadi kelas abstrak juga. 
+ *  Hal ini karena kelas turunan tidak mengimplementasikan seluruh metode abstrak dari kelas induknya 
+ *  sehingga juga menjadi kelas abstrak.
+ *  Jika BujurSangkar tidak mengimplementasikan metode hitungLuas yang dideklarasikan sebagai abstrak 
+ *  pada kelas BangunDatar, maka BujurSangkar juga harus dinyatakan sebagai kelas abstrak. 
+ *  Ini akan menyebabkan kesalahan kompilasi karena tidak mungkin untuk membuat objek dari kelas abstrak.
+ *  Jadi, jika tidak ada implementasi untuk metode abstrak yang ada pada kelas induk, 
+ *  maka kelas turunan juga harus dinyatakan sebagai kelas abstrak.
+ */
